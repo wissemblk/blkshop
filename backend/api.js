d31fde82e9d7e69cd0406b6364d5366ea7607623
wissemblk <wissembelkhodja@gmail.com>
2026-04-24 17:30:28 +0200

    connect railway mysql to vercel

--- backend/api.js
+++ backend/api.js
@@ -2,18 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pool = require('./config/database');
-
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
@@ -28,5 +25,5 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Une erreur est survenue!' });
 });
 
-// Export for Vercel
+// Export for Vercel serverless
 module.exports = app;
